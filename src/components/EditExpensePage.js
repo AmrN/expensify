0f3f9bf2f82a813213f68f93853c2f33df56af84
Amr Noman <amr.noman1990@gmail.com>
2017-11-15 12:41:28 +0300

    add remove expense confirmation dialog

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -2,8 +2,13 @@ import React from 'react';
 import { connect } from 'react-redux';
 import ExpenseForm from './ExpenseForm';
 import { startEditExpense, startRemoveExpense } from '../actions/expenses';
+import Modal from 'react-modal';
 
 export class EditExpensePage extends React.Component {
+  state = {
+    removeDialogOpened: false,
+  }
+
   handleExpenseFormSubmit = (updatedExpense) => {
     this.props.startEditExpense(this.props.expense.id, updatedExpense)
       .then(() => {
@@ -16,6 +21,18 @@ export class EditExpensePage extends React.Component {
       .then(() => {
         this.props.history.push('/');
       });
+  }
+
+  openConfirmationDialog = () => {
+    this.setState(() => ({
+      removeDialogOpened: true,
+    }));
+  }
+
+  closeConfirmationDialog = () => {
+    this.setState(() => ({
+      removeDialogOpened: false,
+    }));
   }
 
   render() {
@@ -34,12 +51,41 @@ export class EditExpensePage extends React.Component {
             onSubmit={this.handleExpenseFormSubmit}
           />
           <button
-            onClick={this.handleRemoveExpense}
-            className="button button--secondary"
+            onClick={this.openConfirmationDialog}
+            className="button button--danger"
           >
             Remove Expense
           </button>
         </div>
+        <Modal
+          isOpen={this.state.removeDialogOpened}
+          className={{
+            base: 'dialog',
+            afterOpen: 'dialog--after-open',
+            beforeClose: 'dialog--before-close',
+          }}
+          overlayClassName={{
+            base: 'dialog-overlay',
+            afterOpen: 'dialog-overlay--after-open',
+            beforeClose: 'dialog-overlay--before-close',
+          }}
+        >
+          <h1 className="dialog-title">Remove this expense?</h1>
+          <div className="dialog-actions">
+            <button
+              onClick={this.handleRemoveExpense}
+              className="button dialog-actions__action"
+            >
+              Okay
+            </button>
+            <button
+              onClick={this.closeConfirmationDialog}
+              className="button button--secondary dialog-actions__action"
+            >
+              Cancel
+            </button>
+          </div>
+        </Modal>
       </div>
     );
   }
